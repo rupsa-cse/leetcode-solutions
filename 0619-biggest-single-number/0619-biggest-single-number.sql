5a95a059a9a6f56f7cc2335b1e93b1c5d1d91f12
Rupsa Pramanik <pramanik.rupsa@gmail.com>
2025-12-14 14:03:15 +0530

    Time: 394 ms (67.21%), Space: 0B (100%) - LeetHub

--- 0619-biggest-single-number/0619-biggest-single-number.sql
+++ 0619-biggest-single-number/0619-biggest-single-number.sql
@@ -1,9 +1,8 @@
 SELECT MAX(num) as num
 FROM MyNumbers
 WHERE num IN (
-    SELECT num
-    FROM MyNumbers
+    SELECT num FROM MyNumbers
     GROUP BY num
-    HAVING COUNT(num) = 1
+    HAVING COUNT(*) = 1
 )
 
