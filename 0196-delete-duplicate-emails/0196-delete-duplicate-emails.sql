9ac8d0cfe4e714aa92a7caf997718c09bee37904
Rupsa Pramanik <pramanik.rupsa@gmail.com>
2025-12-20 10:25:53 +0530

    Time: 461 ms (99.74%), Space: 0B (100%) - LeetHub

---
+++ 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+DELETE FROM Person
+WHERE id IN (
+    SELECT id 
+    FROM(
+        SELECT id,
+        ROW_NUMBER() OVER(PARTITION BY email ORDER BY id) AS row_num
+        FROM Person
+    )t
+    WHERE row_num > 1
+);
